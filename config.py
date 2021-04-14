0a66e7ec5d04c00d863e28a631d306b95c7fa94c
Yura Lytkin <jurasicus@gmail.com>
2021-04-14 19:16:42 +0300

    supernode construction procedure

--- config.py
+++ config.py
@@ -3,11 +3,16 @@ from pathlib import Path
 root_dir = Path(__file__).absolute().parent
 
 data_dir = root_dir / 'data'
-raw_data_dir = data_dir / 'raw'
 
+# raw data
+raw_data_dir = data_dir / 'raw'
 raw_osm_data_fpath = raw_data_dir / 'spb_osm.json.zip'
 raw_routes_data_fpath = raw_data_dir / 'spb_routes.csv'
 
+# preprocessed data
 infrastructure_fpath = data_dir / 'infrastructure.json'
 stops_fpath = data_dir / 'stops.json'
 routes_fpath = data_dir / 'routes.json'
+
+# supernodes
+supernodes_fpath = data_dir / 'supernodes.json'

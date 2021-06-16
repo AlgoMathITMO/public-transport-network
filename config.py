32a68e17c4d43a531c346509b8a1fe169815bf1f
Yura Lytkin <jurasicus@gmail.com>
2021-06-16 18:14:10 +0300

    started restructuring pipeline (features first, clustering second)

--- config.py
+++ config.py
@@ -30,22 +30,20 @@ edges_pspace_fpath = supernodes_dir / 'edges_pspace.json'
 temp_dir = data_dir / 'temp'
 temp_dir.mkdir(exist_ok=True)
 
-subway_edges_pspace_fpath = temp_dir / 'subway_edges_pspace.json'
-
 # infrastructure attributes
 supernode_attributes_fpath = supernodes_dir / 'supernode_attributes.json'
 
+# supernode features
 features_dir = data_dir / 'features'
 features_dir.mkdir(exist_ok=True)
 
 infrastructure_features_fpath = features_dir / 'infrastructure_features.json'
+graph_features_fpath = features_dir / 'graph_features.json'
 
 # clustering
 clustering_dir = data_dir / 'clustering'
 clustering_dir.mkdir(exist_ok=True)
 
 infrastructure_clusters_fpath = clustering_dir / 'infrastructure_clusters.json'
-supernode_clusters_fpath = clustering_dir / 'supernode_clusters.json'
-
-# supernode features
-supernode_features_fpath = features_dir / 'supernode_features.json'
+graph_clusters_fpath = clustering_dir / 'graph_clusters.json'
+consensus_clusters_fpath = clustering_dir / 'consensus_clusters.json'
